b20eaa8ee7b5f80eca60eb4ed4f0e3c82bc6fede
Angel Jesus <230592@utxicotepec.edu.mx>
2025-01-13 21:08:32 -0600

    practica 3

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.use(
         secret: 'RR3-ADJBT#longLive',
         resave: false,
         saveUninitialized: true,
-        cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 horas de sesion
+        cookie: { maxAge: 24 * 60 * 60 * 1000,
+            secure: false 
+
+        } // 24 horas de sesion
     })
 );
 
